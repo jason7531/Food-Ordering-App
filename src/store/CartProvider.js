5d601fef6f9a81c5c791f09b48b6be6dd4ccf953
jason7531 <54910136+jason7531@users.noreply.github.com>
2021-06-10 20:54:08 +0530

    Added context and reducer for cart

---
+++ src/store/CartProvider.js
@@ -0,0 +1,41 @@
+import React, { useReducer } from "react";
+import CartContext from "./cart-context";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    // returns a new array
+    const updatedItems = state.items.concat(action.item);
+  }
+  return defaultCartState;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+  const removeItemHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+  const cartContext = {
+    items: cartState.item,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemHandler,
+    removeItem: removeItemHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
